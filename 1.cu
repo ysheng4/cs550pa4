d7acea0f0d3419f8a6d74b90ac02543c2b8ba64c
ysheng4 <aegeanjie@gmail.com>
2013-11-05 00:00:55 -0600

    Update 1.cu

--- 1.cu
+++ 1.cu
@@ -33,49 +33,46 @@ __device__ char *copy(char *dest, char *src, int n)
 
 
 __global__ void grep(char *myFile, char *myregex, char *result, int line, int width){
+		char *fn = argv[1];
+		char *re = argv[2];
+
     int i = blockDim.x * blockIdx.x + threadIdx.x;
     char *ph;
+
+	 
+    if(re==NULL||fn==NULL){
+        printf("input:file name expression\n");
+        return -1;
+    }
+            
+    for(j = 1; j < 1024; j++){
+        file[j] = file[j-1] + 256;
+		fgets(file[j], 256, f);
+	}
+   
     if(i < line)
     {
         ph = match(&myFile[i*width], myregex);
         if(ph != NULL)
             copy(&result[i*width], &myFile[i*width], sizeof(char)*width);
     }
+
+
 }
 
 int main(int argc, char* argv[])
 {
         char *fn = argv[1];
-    char *re = argv[2];
+		char *re = argv[2];
         char **file;
-    char *result;
+	    char *result;
         FILE *f;
         f = fopen(fn, "r");
         file = (char **)malloc(sizeof(char*)*1024);
-    result = (char *)malloc(sizeof(char)*1024*256);
-    file[0] = (char *)malloc(sizeof(char)*1024*256);        
-    int i;
-    
-    if(re==NULL||fn==NULL){
-        printf("input:file name expression\n");
-        return -1;
-    }
+		result = (char *)malloc(sizeof(char)*1024*256);
+		file[0] = (char *)malloc(sizeof(char)*1024*256);        
+		int i;
    
-        
-    if(f == NULL)
-    {
-        printf("can not open file!\n");
-        return -1;
-    }
-
-            
-    for(i = 1; i < 1024; i++)
-        file[i] = file[i-1] + 256;
-
-    for(i = 0; i < 1024; i++)
-    {
-        fgets(file[i], 256, f);
-    }
 
         // Memory allocation
     char *myfile, *myregex, *myresult;
@@ -91,10 +88,10 @@ int main(int argc, char* argv[])
     cudaMemcpy(result, myresult, sizeof(char)*1024*256, cudaMemcpyDeviceToHost);
     
 
-    for(i = 0; i < 1024; i++)
+    for(j = 0;  j< 1024; j++)
     {
-        if(&result[i*256] != NULL)
-            printf("%s", &result[i*256]);
+        if(&result[j*256] != NULL)
+            printf("%s", &result[j*256]);
     }
 
     return 0;
