39612e3ca917bfaef007151c6bc3abc239032da5
ysheng4 <aegeanjie@gmail.com>
2013-11-05 13:17:55 -0600

    Update gpugrep.cu

--- gpugrep.cu
+++ gpugrep.cu
@@ -37,55 +37,43 @@ __global__ void grep(char *myfile, char *myregex, char *result, int line, int wi
 
 int main(int argc, char* argv[])
 {
-	int i,j=1;
+	int i=1,j=0;
     char *fn = argv[1],*re = argv[2];
     char **file = (char **)malloc(sizeof(char*)*1024);
     char *result= (char *)malloc(sizeof(char)*1024*256);
 	char *myfile, *myregex, *myresult;
     FILE *f;
     f = fopen(fn, "r");
-    file[0] = (char *)malloc(sizeof(char)*1024*256);        
-    
-    
-
-    if(re==NULL||fn==NULL){
+    file[0] = (char *)malloc(sizeof(char)*1024*256);           
+    if(re==NULL||fn==NULL)
+	{
         printf("input string or file");
         return -1;
-    }
-   
-            
-    while(j<1024)
+    }               
+    while(i<1024)
 	{
-        file[j] = file[j-1] + 256;
-
-        fgets(file[j], 256, f);
-		j++;
+        file[i] = file[i-1] + 256;
+        fgets(file[i], 256, f);
+		i++;
 	}
-
-        // Memory allocation
-    
+// Memory allocation   
     cudaMalloc((void**) &myfile, sizeof(char)*1024*256);
     cudaMalloc((void**) &myregex, strlen(re));
     cudaMalloc((void**) &myresult, sizeof(char)*1024*256);
-        // Copying memory to device
+// Copying memory to device
     cudaMemcpy(myfile, &file[0][0], sizeof(char)*1024*256, cudaMemcpyHostToDevice);
     cudaMemcpy(myregex, re,  strlen(re), cudaMemcpyHostToDevice);
-        // Calling the kernel
+ // Calling the kernel
     grep<<<ceil((double)1024/256), 256>>>(myfile, myregex, myresult, 1024, 256);
-        // Copying results back to host
+ // Copying results back to host
     cudaMemcpy(result, myresult, sizeof(char)*1024*256, cudaMemcpyDeviceToHost);
   	cudaFree(myfile);    
 	cudaFree(myregex);
 	cudaFree(myresult);  
-
-    for(i = 0; i < 1024; i++)
+    for(j = 0; j < 1024; j++)
     {
-        if(&result[i*256] != NULL)
-            printf("%s", &result[i*256]);
+        if(&result[j*256] != NULL)
+            printf("%s", &result[j*256]);
     }
-
-
-    return 0;
-	
-        
+    return 0;        
 }
